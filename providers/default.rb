904ff95403bf75d5103a9d7bc1b1de4dd1205f4b
henrymazza <fabio.mazarotto@me.com>
2012-02-09 15:37:58 -0200

    changed installation url

--- providers/default.rb
+++ providers/default.rb
@@ -3,7 +3,7 @@ action :install do
   # install rvm system wide
   bash "install RVM" do
     user   "root"
-    code   %{ bash < <( curl -s https://rvm.beginrescueend.com/install/rvm ) }
+    code   %{ bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer) }
     not_if %{ test -f /usr/local/rvm/bin/rvm }
   end
 
